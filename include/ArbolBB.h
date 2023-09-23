18c79d47f0433b183a72c344763c67f0b9d9825a
rafastrada <57600424+rafastrada@users.noreply.github.com>
2023-09-23 15:43:33 -0300

    Init agregado a ABB

--- include/ArbolBB.h
+++ include/ArbolBB.h
@@ -2,4 +2,20 @@
 #define ARBOLBB_H
 
 
+// definicion incompleta para reconocer el puntero hacia si mismo
+typedef struct hoja ABB_Hoja;
+struct hoja {
+    Envio envio;
+    ABB_Hoja *menores, *mayores;
+};
+
+// Estructura
+typedef struct {
+    ABB_Hoja *raiz;
+} ArbolBB;
+
+
+// Inicializacion de estructura
+void ABB_init(ArbolBB *);
+
 #endif // ARBOLBB_H
--- src/ArbolBB.c
+++ src/ArbolBB.c
@@ -1,1 +1,7 @@
 #include "ArbolBB.h"
+#include <stdlib.h>
+
+void ABB_init(ArbolBB *arbol) {
+    // inicia la raiz del arbol apuntando a un nodo externo
+    arbol->raiz == NULL;
+}
