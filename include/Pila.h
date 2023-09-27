7c877dc3e0b91e0d97ce80cb88dc481007cae0ba
rafastrada <57600424+rafastrada@users.noreply.github.com>
2023-09-27 13:03:23 -0300

    Implementación de Pila
    Implementadas las funciones de la estructura Pila.

--- include/Pila.h
+++ include/Pila.h
@@ -1,6 +1,22 @@
 #ifndef PILA_H
 #define PILA_H
 
+// tipo de elemento de la pila
+#include "ArbolBB.h"
 
+#define PILA_TAM_ARREGLO 300
+
+typedef struct {
+    ABB_Hoja* arreglo[PILA_TAM_ARREGLO];
+    int tope;
+} Pila;
+
+void Pila_init(Pila *);
+
+enum Pila_agregar_retornos {PILA_AGREGAR_EXITO, PILA_AGREGAR_ERROR_LLENA};
+int Pila_agregar(Pila *, ABB_Hoja *);
+
+enum Pila_quitar_retornos {PILA_QUITAR_EXITO, PILA_QUITAR_ERROR_VACIA};
+int Pila_quitar(Pila *, ABB_Hoja **);
 
 #endif // PILA_H
--- src/Pila.c
+++ src/Pila.c
@@ -1,2 +1,30 @@
 #include "Pila.h"
 
+void Pila_init(Pila *pila) {
+    // el tope tiene el indice del ultimo elemento
+    pila->tope = -1;
+}
+
+int Pila_agregar(Pila *pila, ABB_Hoja *puntero) {
+    // si pila no esta llena
+    if (pila->tope < (PILA_TAM_ARREGLO - 1)) {
+        pila->tope++;
+        pila->arreglo[pila->tope] = puntero;
+    }
+    else return PILA_AGREGAR_ERROR_LLENA;
+
+    return PILA_AGREGAR_EXITO;
+}
+
+int Pila_quitar(Pila *pila, ABB_Hoja **puntero) {
+    // guarda en la direccion pasada por parametro el elemento
+    // de la pila
+
+    // si pila no esta vacia
+    if (pila->tope > -1) {
+        *puntero = pila->arreglo[pila->tope];
+        pila->tope--;
+    }
+    else return PILA_QUITAR_ERROR_VACIA;
+    return PILA_QUITAR_EXITO;
+}
